961dacad0a3c53dfb062e1c183ce6f257b7dfd05
Aaron <aaron@macs-MBP.lan>
2020-07-20 19:21:30 +0300

    ne commit

--- src/Components/Accumulate/Accumulate.jsx
+++ src/Components/Accumulate/Accumulate.jsx
@@ -14,34 +14,22 @@ const Accumulate = () => {
 		<Fragment>
 			<section>
 				<div className={`container text-center my-5`}>
-					{width <= 768 ? <div className="introducing">Introducing Ryze</div> : ''}
-					<div
-						className="h3 accumulate py-3"
-						style={{
-							fontSize: width <= 768 ? '100.04px' : '66.04px',
-							marginTop: '4.2rem',
-						}}
-					>
-						Accumulate {width <= 768 ? <br /> : ''} Bitcoin {width <= 768 ? '' : <br />} While{' '}
-						{width <= 768 ? <br /> : ''} you
+					<div className="introducing">Introducing Ryze</div>
+					<div className="h3 accumulate py-3">
+						Accumulate <br className="break-mobile-one" /> Bitcoin <br className="break-web-one" /> While{' '}
+						<br className="break-mobile-one" /> you
 						<span className="change-word">
 							{' '}
 							<ReactTypingEffect text={['Sleep', 'Eat', 'Play', 'Relax']} speed={200} typingDelay={200} />
 						</span>
 					</div>
-					<small
-						ref={(el) => (deposit = el)}
-						className={width <= 768 ? 'animate-deposit-mobile' : 'animate-deposit'}
-						style={{
-							marginTop: `${width <= 768 ? '30px' : '10px'}`,
-							fontWeight: 400,
-							fontSize: width <= 768 ? '22px' : 18,
-						}}
-					>
-						<p className="px-5 my-5">Deposit every week, month or paycheck and {width <= 768 ? <br /> : ''}
+					<small ref={(el) => (deposit = el)}>
+						<p className="px-5 my-5">
+							Deposit every week, month or paycheck and {width <= 768 ? <br /> : ''}
 							<strong> Ryze </strong> will buy the dip for you automatically.
-						<br />
-						Ryze helps you stack sats and build your Bitcoin savings.</p>
+							<br />
+							Ryze helps you stack sats and build your Bitcoin savings.
+						</p>
 					</small>
 					<div className="row justify-content-center" style={{ marginTop: '60px' }}>
 						<div className={`row ${width <= 576 ? '' : 'input1 justify-content-between'}`}>
